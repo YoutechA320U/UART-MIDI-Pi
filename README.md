1fde139dad477b718dfe473788e439bec2f0ab34
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2018-08-16 11:28:25 +0900

    UART-MIDI-Pi
    最初のコミット

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# UART-MIDI-Pi
+RaspberryPi Python2.7用シリアル-MIDI変換プログラム
+
+## 概要
+このプログラムは、RaspberryPiのUARTでMIDIメッセージをやり取りします。現在は入力のみです。GM/GS/XG、その他エクスクルーシブに対応します。
+
+## 必須ライブラリ
+　　pyserial　python-rtmidi　argparse
+
+## 開発環境
+    OS : Raspbian　stretch
+    RaspberryPi : RaspberryPi ZeroWH,RaspberryPi 3B+
+    Python : ver2.7
+
+## 使い方
+できるだけ優先度を上げて実行してください。実行すると"SerialMIDI"という仮想MIDI出力ポートが作成されます。これを他のMIDI入力ポートにつないでください。-dオプション付けて実行すると、出力がコンソールに表示されます。
+
+### 参考コード・資料
+ * <http://www.samplerbox.org/article/midiinwithrpi>  
+ * <https://docs.python.org/ja/2.7/library/functions.html>
+ * <https://qiita.com/unchainendo/items/1ea305d87c797ba02450>  
+ * <https://github.com/SpotlightKid/python-rtmidi>  
+
+## 履歴 / History
---
+++ serialmidi.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# 必須ライブラリ pyerial, python-rtmidi
+import serial
+import rtmidi
+import argparse
+
+# パーサーを作る
+parser = argparse.ArgumentParser(
+            add_help=True, # -h/?help オプションの追加
+            )
+
+# 引数の追加
+parser.add_argument('-d', '--debug', help='show midi message',
+                    action='store_true') #デバッグモード追加
+
+# 引数を解析する
+args = parser.parse_args()
+
+# MIDIポート設定
+midiout = rtmidi.MidiOut()
+midiout.open_virtual_port("SerialMIDI") # 仮想MIDIポートの名前
+ser = serial.Serial('/dev/ttyAMA0', baudrate=38400)　#シリアル読み取り, ボーレート38400kbps
+databyte = 0
+data1 = 0
+data2 = 0
+data3 = 0
+while True:
+  databyte += 1
+  if databyte == 1:
+     data1 = ord(ser.read(1)) # 1byte目
+     if data1 == 247:
+         midiout.send_message([data1]) #0xf7 = 247 があったらここまで出力して1byte目に戻る
+         databyte = 0
+         if args.debug:
+            print ('[{}]'.format(data1))
+         else :
+            pass
+     else:
+         pass
+  if databyte == 2:
+     data2 = ord(ser.read(1)) # 2byte目
+     if data1 >= 192 and data1 <= 208 or data2 == 247: 
+         midiout.send_message([data1,data2]) #3byte目まで無いメッセージがある場合出力して1byte目に戻る
+         databyte = 0
+         if args.debug:
+            print ('[{}, {}]'.format(data1, data2))
+         else :
+            pass
+     else:
+         pass
+  if databyte == 3 :
+     data3 = ord(ser.read(1)) # 3byte目
+     midiout.send_message([data1,data2,data3]) #3byteのメッセージを出力して1byte目に戻る
+     databyte = 0
+     if args.debug:
+        print ('[{}, {}, {}]'.format(data1, data2, data3)) 
+     else :
+        pass
