274c30d4df7980c37a31ea6623364179c045dd4c
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2018-08-18 00:18:55 +0900

    大幅に修正しました。

--- uart-midi.py
+++ uart-midi.py
@@ -20,39 +20,29 @@ args = parser.parse_args()
 midiout = rtmidi.MidiOut()
 midiout.open_virtual_port("SerialMIDI") # 仮想MIDIポートの名前
 ser = serial.Serial('/dev/ttyAMA0', baudrate=38400) #シリアル読み取り, ボーレート38400kbps
-databyte = 0
-data1 = 0
-data2 = 0
-data3 = 0
 while True:
-  databyte += 1
-  if databyte == 1:
-     data1 = ord(ser.read(1)) # 1byte目
-     if data1 == 247:
-         midiout.send_message([data1]) #0xf7 = 247 があったらここまで出力して1byte目に戻る
-         databyte = 0
-         if args.debug:
-            print ('[{}]'.format(data1))
-         else :
-            pass
-     else:
-         pass
-  if databyte == 2:
-     data2 = ord(ser.read(1)) # 2byte目
-     if data1 >= 192 and data1 <= 208 or data2 == 247: 
-         midiout.send_message([data1,data2]) #3byte目まで無いメッセージがある場合出力して1byte目に戻る
-         databyte = 0
-         if args.debug:
-            print ('[{}, {}]'.format(data1, data2))
-         else :
-            pass
-     else:
-         pass
-  if databyte == 3 :
-     data3 = ord(ser.read(1)) # 3byte目
-     midiout.send_message([data1,data2,data3]) #3byteのメッセージを出力して1byte目に戻る
-     databyte = 0
-     if args.debug:
-        print ('[{}, {}, {}]'.format(data1, data2, data3)) 
-     else :
+  data1 = ord(ser.read(1))
+  if data1 == 247:
+      midiout.send_message([data1])
+      if args.debug:
+           print ('[{}]'.format(data1))
+      else:
         pass
+      continue
+  else:
+      data2 = ord(ser.read(1))
+      if data1 >= 192 and data1 <= 208 or data2 == 247:
+         midiout.send_message([data1,data2])
+         if args.debug:
+           print ('[{}, {}]'.format(data1, data2))
+         else :
+           pass
+         continue
+      else:
+         data3 = ord(ser.read(1))
+         midiout.send_message([data1,data2,data3])
+         if args.debug:
+           print ('[{}, {}, {}]'.format(data1, data2, data3)) 
+         else :
+           pass
+         continue        
